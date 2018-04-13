c5c4089ba5541ef039807bf97e571bc1776173e3
lefty8492 <lefty8492@gmail.com>
2018-04-14 01:09:21 +0900

    旧WindowsFormライクな実装を試した

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,33 @@ namespace PrismEasyWpf {
 		public MainWindow() {
 			InitializeComponent();
 		}
+
+		private void TextChangedEvent(object sender, TextChangedEventArgs e) {
+			if (TextBoxContentValidation(ref TxtNum1) && TextBoxContentValidation(ref TxtNum2)) {
+				BtnSum.IsEnabled = true;
+			} else {
+				BtnSum.IsEnabled = false;
+			}
+		}
+
+		private Boolean TextBoxContentValidation(ref TextBox textBox) {
+			double _dummy;
+			if (double.TryParse(textBox.Text, out _dummy)) {
+				textBox.Background = new SolidColorBrush(Colors.White);
+				return true;
+			} else {
+				textBox.Background = new SolidColorBrush(Colors.LightPink);
+				return false;
+			}
+		}
+
+		private void BtnSum_Click(object sender, RoutedEventArgs e) {
+			double num1, num2;
+
+			double.TryParse(TxtNum1.Text, out num1);
+			double.TryParse(TxtNum2.Text, out num2);
+
+			MessageBox.Show((num1 + num2).ToString());
+		}
 	}
 }
