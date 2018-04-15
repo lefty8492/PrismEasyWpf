5f4a0e21c9e8829444d8a6f7e091fd05473e1b7e
lefty8492 <lefty8492@gmail.com>
2018-04-15 15:54:26 +0900

    プレインな状態に戻した、ViewModel.csを追加

---
+++ ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace PrismEasyWpf {
+	class ViewModel : INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
