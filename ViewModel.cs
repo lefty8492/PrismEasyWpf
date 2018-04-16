7bdbb9b0071b337d4c77c4159fa1578d9d041380
lefty8492 <lefty8492@gmail.com>
2018-04-16 23:54:29 +0900

    IDataErrorInfoの素朴な実装

--- ViewModel.cs
+++ ViewModel.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 
 namespace PrismEasyWpf {
-	class ViewModel : INotifyPropertyChanged {
+	class ViewModel : INotifyPropertyChanged, IDataErrorInfo {
 		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string Error {
+			get {
+				return (X < 0) ? "Value is not positive." : null;
+			}
+		}
+
+		public string this[string columnName] {
+			get {
+				switch (columnName) {
+				case "X": return (X < 0) ? "Value is not positive." : null;
+				}
+				return null;
+			}
+		}
 
 		double _x = 0;
 
